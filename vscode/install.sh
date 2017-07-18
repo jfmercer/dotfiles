5bfd7a88ded6fdcf495a27dafb22ccb0c8489a10
John F. Mercer <john.f.mercer@gmail.com>
2017-07-17 21:26:02 -0400

    Add vscode to dotfiles

---
+++ vscode/install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+if test "$(which code)"; then
+  if [ "$(uname -s)" = "Darwin" ]; then
+    VSCODE_HOME="$HOME/Library/Application\ Support/Code/User"
+  else
+    VSCODE_HOME="$HOME/.config/Code"
+  fi
+
+  ln -sf "$DOTFILES/vscode/settings.json" "$VSCODE_HOME/User/settings.json"
+  ln -sf "$DOTFILES/vscode/keybindings.json" "$VSCODE_HOME/User/keybindings.json"
+
+  # from `code --list-extensions`
+  modules="
+    EditorConfig.EditorConfig
+    PeterJausovec.vscode-docker
+    Zignd.html-css-class-completion
+    christian-kohler.npm-intellisense
+    christian-kohler.path-intellisense
+    dbaeumer.vscode-eslint
+    dracula-theme.theme-dracula
+    eg2.tslint
+    eg2.vscode-npm-script
+    johnpapa.Angular2
+    lkytal.FlatUI
+    mkaufman.HTMLHint
+    msjsdiag.debugger-for-chrome
+    shinnn.stylelint
+"
+  for module in $modules; do
+    code --install-extension "$module" || true
+  done
+fi
