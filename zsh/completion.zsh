47d54f3385d38c9e57efe5083eb1bdd44826ddfc
John F. Mercer <john.f.mercer@gmail.com>
2017-08-06 11:26:12 -0400

    Activate zsh caching

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -5,3 +5,12 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# Caching = speed
+zstyle ':completion:*' accept-exact '*(N)'
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path $HOME/.zcompcache
+
+# Look at
+# https://github.com/sorin-ionescu/prezto/blob/master/modules/completion/init.zsh
+#
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -2,28 +2,22 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-# don't nice background tasks
-setopt NO_BG_NICE
+
+setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
 setopt NO_LIST_BEEP
-# allow functions to have local options
-setopt LOCAL_OPTIONS
-# allow functions to have local traps
-setopt LOCAL_TRAPS
-# add timestamps to history
-setopt EXTENDED_HISTORY
+setopt LOCAL_OPTIONS # allow functions to have local options
+setopt LOCAL_TRAPS # allow functions to have local traps
+setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
-setopt CORRECT
+setopt CORRECT # correct commands
 setopt COMPLETE_IN_WORD
-# adds history
-setopt APPEND_HISTORY
-# adds history incrementally and share it across sessions
-setopt INC_APPEND_HISTORY
+setopt APPEND_HISTORY # adds history
+setopt INC_APPEND_HISTORY # adds history incrementally and share it across sessions
 setopt SHARE_HISTORY
-# don't record dupes in history
-setopt HIST_IGNORE_ALL_DUPS
-setopt HIST_REDUCE_BLANKS
+setopt HIST_IGNORE_ALL_DUPS # don't record dupes in history
 setopt HIST_IGNORE_DUPS
+setopt HIST_REDUCE_BLANKS
 setopt HIST_IGNORE_SPACE
 setopt HIST_VERIFY
 setopt HIST_EXPIRE_DUPS_FIRST
