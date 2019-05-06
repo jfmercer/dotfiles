4e4a1e6d9a07ef158d1df4b304eb3bb800c7ba37
John F. Mercer <jmercer@clarivoy.com>
2019-05-06 11:05:40 -0400

    Disable pure prompt's auto git pulls

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,2 +1,5 @@
 #!/bin/zsh
+# The max execution time of a process before its run time is shown when it exits.
 PURE_CMD_MAX_EXEC_TIME=1
+# Prevents Pure from checking whether the current Git remote has been updated.
+PURE_GIT_PULL=0
