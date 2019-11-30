16dd738e1b27c144b947be75534d332a8ffaf9a5
John F. Mercer <jfmercer@protonmail.com>
2019-11-30 11:23:59 -0500

    Move nvm stuff to node dir

--- node/exports.zsh
+++ node/exports.zsh
@@ -1,1 +1,4 @@
-export NVM_SYMLINK_CURRENT=true
+# export NVM_SYMLINK_CURRENT=true
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
