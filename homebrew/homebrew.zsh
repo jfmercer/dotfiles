5b209bc2660f222d4112b48d5205f68a5e78d4cd
John F. Mercer <jfmercer@protonmail.com>
2023-02-24 19:28:59 -0500

    Fix asdf PATH problem

--- homebrew/homebrew.zsh
+++ homebrew/homebrew.zsh
@@ -2,9 +2,13 @@
 # macOS $HOME dir custom install
 if [ -d "$HOME/homebrew" ]; then
     eval "$($HOME/homebrew/bin/brew shellenv)"
+    # Put asdf before homebrew in $PATH when homebrew is in $HOME/homebrew
+    . $HOME/.asdf/asdf.sh
 # linuxbrew install
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    # Put asdf before homebrew in $PATH on linuxbrew
+    . $HOME/.asdf/asdf.sh
 # standard macOS install
 else
     eval "$(/usr/local/bin/brew shellenv)"
