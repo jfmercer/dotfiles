1c9d7c7c0cbc55a5f1a4bcbf3518c4acc3b600ff
John F. Mercer <jfmercer@protonmail.com>
2023-02-24 19:28:59 -0500

    Fix asdf PATH problem

--- asdf/asdf.zsh
+++ asdf/asdf.zsh
@@ -1,4 +1,12 @@
-. $HOME/.asdf/asdf.sh
+# Cheap hack to get $PATH to work for custom homebrew dir
+# See homebrew.zsh for asdf getting loaded in that situation
+# asdf needs to be before homebrew in the $PATH
+#
+# Please see homebrew/homebrew.zsh for more sadness
+#
+if [ ! -d "$HOME/homebrew" ] && [ ! -d "/home/linuxbrew/" ]; then
+    . $HOME/.asdf/asdf.sh
+fi
 
 # append completions to fpath
 fpath=(${ASDF_DIR}/completions $fpath)
