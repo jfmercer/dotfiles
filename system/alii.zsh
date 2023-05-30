76ad2ceaca871955306079f3355ed14ac9eeef24
John F. Mercer <jfmercer@protonmail.com>
2023-05-30 10:16:31 -0400

    Colorize less and exa

--- system/alii.zsh
+++ system/alii.zsh
@@ -60,14 +60,11 @@ alias unixtime="date +%s"
 # File size
 alias fs="stat -c \"%s bytes\""
 
-# weather
-alias we="curl http://wttr.in/ | less -r"
-
-# dockerize postgres
-alias psql="docker run --rm -it --net=host postgres:12.1 psql"
-
 # chezmoi
 alias cm=chezmoi
 
 # arsenal
 alias a=arsenal
+
+# colorize less
+alias less="less -R"
--- system/ls.zsh
+++ system/ls.zsh
@@ -1,10 +1,10 @@
 # Always use exa, the better ls
-alias ls="command exa"
+alias ls="command exa --color=always"
 # List only directories
 alias lsd="ls -lFD"
 
 # List all files colorized in long format
-alias l="ls -lgF ${colorflag}"
+alias l="ls -lgF"
 # List all files colorized in long format, including dot files
-alias la="ls -lagF ${colorflag}"
+alias la="ls -lagF"
 
