885da0256385369c59fb1423ca5a400879357069
John F. Mercer <jfmercer@protonmail.com>
2023-11-15 19:30:04 -0300

    Install direnv; fix grc

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,2 +1,6 @@
 # GRC colorizes nifty unix tools all over the place
-source $HOMEBREW_PREFIX/etc/grc.zsh
+if type brew &>/dev/null; then
+  source $HOMEBREW_PREFIX/etc/grc.zsh
+else
+  source /etc/grc.zsh
+fi
