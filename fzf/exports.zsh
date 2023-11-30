c695ae049fd15a90c8c9c1e72b31ee5d929a54c8
John F. Mercer <jfmercer@protonmail.com>
2023-11-30 15:22:43 -0500

    Fix fzf

--- fzf/exports.zsh
+++ fzf/exports.zsh
@@ -8,5 +8,10 @@ export FZF_DEFAULT_COMMAND="git ls-files --cached --others --exclude-standard | 
 export FZF_CTRL_T_COMMAND="fd $FD_OPTIONS"
 export FZF_ALT_C_COMMAND="fd --type d $FD_OPTIONS"
 
-export BAT_PAGER="less -R"
-
+# On Linux, we need fdfind instead of fd and batcat instead of bat.
+if [[ "$(uname)" == "Linux" ]]; then
+    export FZF_DEFAULT_OPTS="--no-mouse --height 90% -1 --reverse --multi --inline-info --preview='[[ \$(file --mime {}) =~ binary ]] && echo {} is a binary file || (batcat --style=numbers --color=always {} || cat {}) 2> /dev/null | head -300'"
+    export FZF_DEFAULT_COMMAND="git ls-files --cached --others --exclude-standard | fdfind --type f --type l $FD_OPTIONS"
+    export FZF_CTRL_T_COMMAND="fdfind $FD_OPTIONS"
+    export FZF_ALT_C_COMMAND="fdfind --type d $FD_OPTIONS"
+fi
