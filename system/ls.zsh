c571676584d4bdccb81476305c735c1f0a64e20f
John F. Mercer <jfmercer@protonmail.com>
2024-11-22 07:23:17 -0500

    Change alias

--- system/ls.zsh
+++ system/ls.zsh
@@ -6,5 +6,5 @@ alias lsd="ls -lFD"
 # List all files colorized in long format
 alias l="ls -lgF"
 # List all files colorized in long format, including dot files
-alias la="ls -lagF"
+alias ll="ls -lagF"
 
