e32dcbef240fe7962875f632efa866ece8d13ff0
John F. Mercer <jfmercer@protonmail.com>
2026-01-21 15:58:01 -0500

    Update asdf installation

--- asdf/asdf.zsh
+++ asdf/asdf.zsh
@@ -1,13 +1,16 @@
-# Cheap hack to get $PATH to work for custom homebrew dir
-# See homebrew.zsh for asdf getting loaded in that situation
-# asdf needs to be before homebrew in the $PATH
-#
-# Please see homebrew/homebrew.zsh for more sadness
-#
-if [ ! -d "$HOME/homebrew" ] && [ ! -d "/home/linuxbrew/" ]; then
-    . $HOME/.asdf/asdf.sh
+# https://asdf-vm.com/guide/getting-started.html#_1-install-asdf
+
+completions_dir="${ASDF_DATA_DIR:-$HOME/.asdf}/completions"
+asdf_completion_file="${completions_dir}/_asdf"
+
+if [[ ! -d "$completions_dir" ]]; then
+  mkdir -p "$completions_dir"
+fi
+
+if [[ ! -f "$asdf_completion_file" ]]; then
+  asdf completion zsh > "$asdf_completion_file"
 fi
 
 # append completions to fpath
-fpath=(${ASDF_DIR}/completions $fpath)
+fpath=(${ASDF_DATA_DIR:-$HOME/.asdf}/completions $fpath)
 # compinit happens in zshrc and is not needed here
