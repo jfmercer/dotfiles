09b2a4170a9d0ce673c3f56a681fca852e03af0e
John F. Mercer <jfmercer@protonmail.com>
2026-02-24 09:52:55 -0500

    Tweak homebrew settings

--- homebrew/homebrew.zsh
+++ homebrew/homebrew.zsh
@@ -1,12 +1,15 @@
 # initialize homebrew
+# if [[ "$(uname -s)" == "Darwin" ]]; then
+#     # macOS $HOME dir custom install
+#     if [ -d "$HOME/homebrew" ]; then
+#         eval "$($HOME/homebrew/bin/brew shellenv)"
+#         # Put asdf before homebrew in $PATH when homebrew is in $HOME/homebrew
+#         . $HOME/.asdf/asdf.sh
+#     # standard macOS install
+#     else
+#         eval "$(/opt/homebrew/bin/brew shellenv)"
+#     fi
+# fi
 if [[ "$(uname -s)" == "Darwin" ]]; then
-    # macOS $HOME dir custom install
-    if [ -d "$HOME/homebrew" ]; then
-        eval "$($HOME/homebrew/bin/brew shellenv)"
-        # Put asdf before homebrew in $PATH when homebrew is in $HOME/homebrew
-        . $HOME/.asdf/asdf.sh
-    # standard macOS install
-    else
-        eval "$(/opt/homebrew/bin/brew shellenv)"
-    fi
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
