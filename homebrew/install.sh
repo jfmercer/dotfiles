b2e3a1a1ea119d1f50212f3a9c24fd1f9ce1ed96
John F. Mercer <jfmercer@protonmail.com>
2026-07-01 11:52:08 -0400

    Fix a bunch of small problems

--- homebrew/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Install `brew` in $HOME rather than /usr/local
-git clone https://github.com/Homebrew/brew "$HOME"/homebrew
-eval "$HOME/homebrew/bin/brew shellenv"
-brew update --force --quiet
-chmod -R go-w "$(brew --prefix)/share/zsh"
